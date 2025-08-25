e826b98d2339e39cec48af43f9893bd715922cff
uller91 <girnikis@gmail.com>
2025-08-24 21:23:39 -0700

    assignment 2-2 - broken test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,56 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestGetApiKet(t *testing.T) {
+	req, _ := http.NewRequest("GET", "", nil)
+	token := "Bearer hopuga"
+	req.Header.Set("Authorization", token)
+	
+	req2, _ := http.NewRequest("GET", "", nil)
+
+	req3, _ := http.NewRequest("GET", "", nil)
+	token2 := "ApiKey bruh"
+	req3.Header.Set("Authorization", token2)
+
+	tests := []struct {
+		name    string
+		token 	string
+		header  http.Header
+		wantErr bool
+	}{
+		{
+			name:     "Wrong header token",
+			token: 		"hopuga",
+			header:     req.Header,
+			wantErr:  	true,
+		},
+		{
+			name:     "No header",
+			token: 		"",
+			header:     req2.Header,
+			wantErr:  	true,
+		},
+		{
+			name:     "Correct header",
+			token: 		"bruh",
+			header:     req3.Header,
+			wantErr:  	false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ret, err := GetAPIKey(tt.heade)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAPIKey() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if (err == nil) && ret != tt.token {
+				t.Errorf("GetAPIKey() wrong token, token %v, return %v", token, ret)
+			}
+		})
+	}
+}
