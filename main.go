a8d84a97b06bc5a24bbd4752c8325bed9609c03f
uller91 <girnikis@gmail.com>
2025-08-28 16:26:00 -0700

    assignment 4-2 fail 2

--- main.go
+++ main.go
@@ -22,10 +22,9 @@ type apiConfig struct {
 }
 
 func unused() {
-    // this function does nothing
-    // and is called nowhere
+	// this function does nothing
+	// and is called nowhere
 }
-
 
 //go:embed static/*
 var staticFiles embed.FS
