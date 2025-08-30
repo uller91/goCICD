e199454c572a91000ab78327f1f2bc8355030981
uller91 <girnikis@gmail.com>
2025-08-29 22:15:57 -0700

    assignment 5-3

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -88,9 +89,11 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 
 	router.Mount("/v1", v1Router)
+	timeout, _ := time.ParseDuration("1s")
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: timeout,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
